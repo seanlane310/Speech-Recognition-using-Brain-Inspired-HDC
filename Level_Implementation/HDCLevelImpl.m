2383aeac6b450a27aef7b521a7bd3ce305d55a3c
seanlane310 <58016223+seanlane310@users.noreply.github.com>
2022-04-11 18:47:50 -0400

    Add files via upload
    MATLAB Level Implementation Files

---
+++ Level_Implementation/HDCLevelImpl.m
@@ -0,0 +1,251 @@
+%%Sean Lane
+%Hyperdimensional Computing Speech Recognition
+%Thesis Research
+%Villanova University
+%Fall 2021 - Spring 2022
+
+
+%% Training Phase Execution
+%tic
+bins = 75;
+dim = 2880;
+
+%use if need to generate HVs
+[digitHV, posHV] = GetBaseHVs(bins);
+num_epochs = 20;
+
+%load preprocessed
+load('C:\Users\seanl\Documents\Research\bins_sets.mat');
+
+% Set global variables
+
+classes = [{'down'},{'eight'},{'five'},{'four'}];
+% classes = [{'bed'},{'bird'},{'cat'},{'dog'},{'down'},{'eight'}...
+%     {'five'},{'four'},{'go'},{'happy'},{'house'},{'left'},{'marvel'}...
+%     {'nine'},{'no'},{'off'},{'on'},{'one'},{'right'},{'seven'}...
+%     {'sheila'},{'six'},{'stop'},{'three'},{'tree'},{'two'},{'up'}...
+%     {'wow'},{'yes'},{'zero'}];                         %Different classes
+
+num_classes = length(classes);
+total_records = 100;                                     %Number of audio samples for each class
+kFold = 5;
+test_records = total_records/kFold;                      %To use 20% as testing samples
+sample_records = total_records - test_records;
+
+indices = crossvalind('Kfold',total_records,kFold);      %5-fold crossvalind
+
+
+maxFileLength = 16384;                                   %the files of this dataset are 16000, use 16384 for mult of 2 (fft)
+
+for foldIDX = 1:kFold
+fprintf('Fold: %d \n',foldIDX);
+fprintf('\n');
+    
+Test_idx = (indices == foldIDX);                          %Generating random indexes to choose test samples
+totalCounts = zeros(length(classes),1);                   %Store total samples executed
+sampleCounts = zeros(length(classes),1);                  %Store training samples executed
+testCounts = zeros(length(classes),1);                    %Store test samples executed
+bins_train_sets = zeros(sample_records,bins,num_classes); %Bins for the training samples
+bins_test_sets = zeros(test_records,bins,num_classes);    %Bins or the testing samples
+
+
+%For loop to split samples into testing and training based on random split
+%80/20
+    for i = 1:num_classes
+        for k = 1:100
+                totalCounts(i) = totalCounts(i) + 1;
+                if(Test_idx(totalCounts(i),1) == 1)
+                    testCounts(i) = testCounts(i) + 1;
+                    bins_test_sets(testCounts(i),:,i) = bins_sets(k,:,i);
+                else
+                    sampleCounts(i) = sampleCounts(i) + 1;
+                    bins_train_sets(sampleCounts(i),:,i) = bins_sets(k,:,i);
+                end
+        end
+    end
+
+%Stores encoded hypervectors for sample and test samples
+enc_HVs = zeros(sample_records,dim,num_classes);
+enc_test_HVs = zeros(test_records,dim,num_classes);
+
+tic
+%Sending bins for training and testing to be audio encoded then stored in
+%respected matrices
+        for i = 1:length(classes)
+                for j = 1:sample_records
+                    enc_HVs(j,:,i) = AudioEncoder(bins_train_sets(j,:,i), digitHV, posHV);
+                end
+                for k = 1:test_records
+                    enc_test_HVs(k,:,i) = AudioEncoder(bins_test_sets(k,:,i), digitHV, posHV);
+                end
+        end 
+
+
+% if even, subtract 1; if odd, leave be
+%last_hv = ((sample_records - 1) + mod(sample_records,2));
+%Hardcoded for 80  sample records
+last_hv = 79;
+
+%Matrix to store Class Hypervectors
+class_HVs = Train(enc_HVs(1:last_hv,:,:),dim);
+%Second matrix to store next class HV values between retraining
+next_epoch_class_HVs = class_HVs;
+toc
+%% Sample inference test
+test_HVs = enc_test_HVs;
+%total = 0;
+%correct = 0;
+
+%Number of passes through dataset - retraining done in each epoch
+for l=1:num_epochs
+
+%counts for total and corret predictions
+correct = 0; 
+total = 0;
+
+class_HVs = next_epoch_class_HVs;
+
+check = zeros(1,num_classes);
+difference_array = zeros(2880,1);
+classHV_Manhattan_distance = zeros(length(classes),test_records,length(classes));
+for j = 1:length(classes)
+    for i = 1:test_records
+        for k = 1:length(classes)
+            %Get manhattan distancefrom each class HV to the current sample
+            %test HV
+            classHV_Manhattan_distance(k,i,j) = sum(abs(class_HVs(k,:) - test_HVs(i,:,j)));
+            check(k) = classHV_Manhattan_distance(k,i,j);
+            if(k == length(classes))
+                %Check which class had lowest Manhattan distance
+                total = total + 1;
+                [M,I] = min(check);
+                if ( I == j)
+                    correct = correct + 1;
+                else
+                    %difference array stores the difference between each
+                    %class and the test HV, this is / by 80 (for number of
+                    %samples) and then added to incorrectly predicted class
+                    %HV,and subtracted from the correct class HV
+                    difference_array = (class_HVs(I,:) - test_HVs(i,:,j))/sample_records;
+                    difference_array = difference_array;
+                    %difference_array = floor(difference_array);
+                    %subtract the hypervector from the correct guess
+                    %class HV, add it to the incorrect HV. Correct is j,
+                    %incorrect is I
+                    next_epoch_class_HVs(j,:) = next_epoch_class_HVs(j,:) - difference_array;
+                    next_epoch_class_HVs(I,:) = next_epoch_class_HVs(I,:) + difference_array;
+                end
+                
+            end
+        end
+    end
+    %fprintf("%i\n\n", sum(xor(class_HVs(i,:),class_HVs((mod(i,(classes/2))+1),:)))); 
+end
+    fprintf('Number of Bins: %d Number Correct: %d / %d Epoch Number: %d \n',bins, correct,total, l);
+    fprintf('\n');
+
+end 
+end
+%toc
+
+
+function [level_HVs, pos_HVs] = GetBaseHVs(num_bins)
+%% Initial Hypervector Generation
+% Gives 2 hypervector sets: the 10 digit vectors and a number of position
+%       vectors dependent on the number of audio bins.
+%
+% Takes num_bins, the number of bins of the audio samples.
+%
+% Returns level_HVs, the set of 10 hypervectors corresponding to the
+%       numbers 0 through 9.
+% Returns pos_HVs, the set of hypervectors corresponding to the
+%       positions of the bins of the audio sample.
+
+    D = 2880;
+    %M Is used for the randi function to decide number of bits to flip in
+    %HV
+    M = 10;
+   
+    lev_HVs(1,:) = randi([0,1],1,D);
+
+    %High accuracy, but uncorrelated levels
+    %Not for VHDL implementation because uses randi
+
+    for d = 2:13%117
+        lev_HVs(d,:) = lev_HVs(d-1,:);
+        rand = randi(D,(D/M),1);
+        for i = 1:length(rand)
+            lev_HVs(d,rand(i)) = ~lev_HVs(d,rand(i));
+        end
+    end
+
+%Correlated HVs, not as accurate and the randi level HVs but easier to
+%implement in VHDL and proven in previous HDC implementations to be
+%effective
+%     for d = 2:13%117
+%         lev_HVs(d,:) = lev_HVs(d-1,:);
+%         for i = 1:216 %24
+%             lev_HVs(d,((d-2)*216)+i) = ~lev_HVs(d,((d-2)*216)+i); %24
+%         end
+%     end
+       
+    
+    ID_HVs = zeros(num_bins,D); 
+    for p = 1:num_bins
+        ID_HVs(p,:) = randi([0,1],1,D);
+    end
+    
+    level_HVs = lev_HVs;
+    pos_HVs = ID_HVs;
+
+end
+
+
+function [audio_HV] = AudioEncoder(bin_values, level_HVs, pos_HVs)
+%% Audio-Hypervector Encoding
+% Encodes the audio sample into one hypervector using the audio bins and
+%       digit and bin position hypervectors.
+%256
+% Takes bin_values, the set of decimal numbers representing the average
+%       frequency values of one audio sample.
+% Takes level_HVs, the set of 10 hypervectors, each corresponding to a
+%       digit (the number 0 through 9).
+% Takes pos_HVs, the set of hypervectors corresponding to the positions of
+%       the bins of the audio sample.
+%
+% Returns audio_HV, the hypervector representation of the audio sample
+
+    audlevel_HVs = zeros(75,2880);  %75
+    for i = 1:length(bin_values)
+        %audlevel_HVs(i,:) = BinEncoder(bin_values(i), level_HVs);
+        audlevel_HVs(i,:) = level_HVs((bin_values(i)+1), :);
+        %audlevel_HVs(i,:) = level_HVs(bin_values(i), :);
+    end
+    
+    audio_HV = audlevel_HVs(1,:) .* pos_HVs(1,:);
+    for i = 2:size(audlevel_HVs,1)
+        audio_HV = audio_HV + (audlevel_HVs(i,:) .* pos_HVs(i,:));
+    end
+end
+
+
+function [class_HVs] = Train(HV_sets,dim)
+%% Training Function
+% Takes HV_sets, a three-dimensional array that represents the full set of
+%       all of the hypervectors, where each is a one-dimensional array, and
+%       they are organized into two-dimensional arrays based on the class.
+%   
+% Returns class_HVs, a two-dimensional array of the trained class
+%       hypervectors that are represented as one-dimensional arrays
+class_HVs = zeros(size(HV_sets,3),dim);
+    for i = 1:size(HV_sets,3)
+        class_set = HV_sets(:,:,i);
+        div_num = (size(class_set,1) + 1);
+        
+        for j = 1:size(class_set,2)
+            col_sum = sum(class_set(:,j));
+            class_HVs(i,j) = fix(abs(col_sum/div_num));
+        end
+    end
+
+end
---
+++ Level_Implementation/HDCPreProcessing.m
@@ -0,0 +1,205 @@
+%%Sean Lane
+%Hyperdimensional Computing Speech Recognition
+%Thesis Research
+%Villanova University
+%Fall 2021 - Spring 2022
+
+bins = 75;
+
+%use if need to generate HVs
+%[digitHV, posHV] = GetBaseHVs(bins);
+
+% Set global variables
+
+classes = [{'down'},{'eight'},{'five'},{'four'}];
+% classes = [{'bed'},{'bird'},{'cat'},{'dog'},{'down'},{'eight'}...
+%     {'five'},{'four'},{'go'},{'happy'},{'house'},{'left'},{'marvel'}...
+%     {'nine'},{'no'},{'off'},{'on'},{'one'},{'right'},{'seven'}...
+%     {'sheila'},{'six'},{'stop'},{'three'},{'tree'},{'two'},{'up'}...
+%     {'wow'},{'yes'},{'zero'}];                         %Different classes
+num_classes = length(classes);
+
+
+% for loading files into a 3-D matrix
+% matrix declaration
+maxFileLength = 16384; %the files of this dataset are 16000, use 16384 for mult of 2 (fft)
+audio_sets = zeros(16384,100,length(classes));
+totalCounts = zeros(length(classes),1);
+sampleCounts = zeros(length(classes),1);
+bins_sets = zeros(100,bins,num_classes);
+
+
+%loading each .wav file, converting to .mat, storing in mx, running 
+MyDir = 'C:\Users\seanl\Documents\Research\SingleWordsSmall\';
+    for i = 1:length(classes)
+        name = strcat(MyDir,char(classes(i)), '\');
+        files1 = dir(name);
+        for k = 3:length(files1)                         % looping through records of same motions
+            path_name = strcat(name,files1(k).name); 
+                totalCounts(i) = totalCounts(i) + 1;
+                [sig,fs] = audioread(path_name);
+                if(length(sig) < maxFileLength)
+                    sig(end:maxFileLength)=0;
+                end
+                sampleCounts(i) = sampleCounts(i) + 1;
+                audio_sets(:,sampleCounts(i),i) = sig;
+        end
+    end
+        
+        for i = 1:length(classes)
+            bins_sets(:,:,i) = GetFreqBins(audio_sets(:,:,i), bins);
+        end
+        
+
+
+%% Save Features to savDir directory 
+savdir = 'C:\Users\seanl\Documents\Research\';
+
+
+
+%bins_sets = rescale(bins_sets,0,116);
+bins_sets = round(bins_sets);
+save(fullfile(savdir, 'bins_sets'), 'bins_sets');
+        
+        
+function [bin_values] = GetFreqBins(audioData, num_bins)
+%% Frequency Bin Generation
+% Turns audio samples into sets of frequency bins.
+%
+% Takes audioData, the audio samples.
+% Takes num_bins, the number of bins of the audio samples.
+%
+% Returns bin_values, the set of bin values split into sections.
+
+    num_takes = size(audioData,2);
+
+    X = zeros(16384,num_takes);
+    %X = zeros(num_bins,num_takes);
+    for i=1:num_takes
+        X(:,i) = fft(audioData(:,i));
+        %X(:,i) = fft(audioData(:,i),num_bins);
+    end
+     
+    Z = zeros(16000,num_takes);
+    %Z = zeros(num_bins,num_takes);
+    for s = 1:num_takes
+        Z(:,s) = abs(X(1:16000,s));
+        %Z(:,s) = abs(X(1:num_bins,s));
+    end
+    
+    total = length(1:length(Z(:,1)));
+    
+    binsize = total/num_bins;
+    
+    average = zeros(num_takes,num_bins);
+    tiers = zeros(num_bins,1);
+
+
+    for s = 1:num_takes
+        for i = 1:num_bins
+            if ( i == 1)
+                average(s,i) = mean(0:Z(round((i*binsize)+1),s));
+            else
+                average(s,i) = mean(Z(round(((i-1)*binsize)+1):round((i)*binsize),s));
+            end
+        end
+    end
+
+ %average = average * 10;
+
+%     increaserate = 1.13;
+% 
+%     %tiers(1,1) = 0.001;
+%     tiers(1,1) = 0.01;
+%     for s = 2:num_bins
+%         tiers(s,1) = tiers(s-1,1) * increaserate;
+%     end
+% 
+%      for s = 1:num_takes
+%         for i = 1:num_bins
+%             k = 1;
+%             while((average(s,i) > tiers(k,1)) && (k < num_bins))
+%                 k = k + 1;
+%             end
+%             if(average(s,i) < tiers(k,1))
+%                 average(s,i) = tiers(k,1);
+%             else 
+%                 average(s,i) = tiers(num_bins,1);
+%             end
+%         end
+%     end
+
+%      for s = 1:num_takes
+%         for i = 1:num_bins
+%             k = 1;
+%             while((Z(i,s) > tiers(k,1)) && (k < num_bins))
+%                 k = k + 1;
+%             end
+%             if(Z(i,s) < tiers(k,1))
+%                 Z(i,s) = k;
+%             else 
+%                 Z(i,s) = num_bins;
+%             end
+%         end
+%     end
+       
+     bin_values = average;
+      %Z=Z';
+      %bin_values = Z;
+% 
+%     for s = 1:num_takes
+%         for i = 1:num_bins
+%             if (Z(i,s) < 0.025)
+%                 bin_values(s,i) = 1;
+%             elseif (Z(i,s) < 0.05)
+%                 bin_values(s,i) = 2;
+%             elseif (Z(i,s) < 0.075)
+%                 bin_values(s,i) = 3;
+%             elseif (Z(i,s) < 0.1)
+%                 bin_values(s,i) = 4;
+%             elseif (Z(i,s) < 0.125)
+%                 bin_values(s,i) = 5;
+%             elseif (Z(i,s) < 0.15)
+%                 bin_values(s,i) = 6;
+%             elseif (Z(i,s) < 0.175)
+%                 bin_values(s,i) = 7;
+%             elseif (Z(i,s) < 0.2)
+%                 bin_values(s,i) = 8;
+%             elseif (Z(i,s) < 0.25)
+%                 bin_values(s,i) = 9;
+%             elseif (Z(i,s) < 0.3)
+%                 bin_values(s,i) = 10;
+%             elseif (Z(i,s) < 0.35)
+%                 bin_values(s,i) = 11;
+%             elseif (Z(i,s) < 0.4)
+%                 bin_values(s,i) = 12;
+%             elseif (Z(i,s) < 0.5)
+%                 bin_values(s,i) = 13;
+%             elseif (Z(i,s) < 0.6)
+%                 bin_values(s,i) = 14;
+%             elseif (Z(i,s) < 0.7)
+%                 bin_values(s,i) = 15;
+%             elseif (Z(i,s) < 0.8)
+%                 bin_values(s,i) = 16;
+%             elseif (Z(i,s) < 0.9)
+%                 bin_values(s,i) = 17;
+%             elseif (Z(i,s) < 1)
+%                 bin_values(s,i) = 18;
+%            elseif (Z(i,s) < 1.25)
+%                 bin_values(s,i) = 19;
+%            elseif (Z(i,s) < 1.5)
+%                 bin_values(s,i) = 20;
+%              elseif (Z(i,s) < 2)
+%                 bin_values(s,i) = 21;
+%              elseif (Z(i,s) < 2.5)
+%                 bin_values(s,i) = 22;
+%              elseif (Z(i,s) < 3)
+%                 bin_values(s,i) = 23;
+%             elseif (Z(i,s) < 5)
+%                 bin_values(s,i) = 24;
+%             else
+%                 bin_values(s,i) = 25;
+%             end
+%         end
+%    end
+end
